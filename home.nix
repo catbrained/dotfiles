74b50f2d5a6d125c6f0a2421079f5fe2bb2f3c2f
Linda Siemons <linda@catbrained.dev>
2023-11-09 03:59:15 +0100

    feat: configure git via Home Manager
    The HM module for git seems to not support some
    of the settings I want to use yet:
    
    - `init.defaultBranch`
    - Signing with SSH keys
      The module has support for signing with *GPG*, but apparently not SSH.
      If I use the options to enable commit and tag signing by default HM
      will also add a `gpg.program` setting pointing to gpg. I'm not sure
      if it would break anything to have it there. I enabled commit and tag
      signing via the extra options, just to be sure.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home.nix
+++ home.nix
@@ -4,6 +4,21 @@
   home.username = "linda";
   home.homeDirectory = "/home/linda";
 
+  programs.git = {
+    enable = true;
+    userEmail = "linda@catbrained.dev";
+    userName = "Linda Siemons";
+    extraConfig = {
+      # XXX: The HM module does not yet support defaultBranch
+      init.defaultBranch = "main";
+      # XXX: The HM module does not yet support ssh signing
+      commit.gpgSign = true;
+      tag.gpgSign = true;
+      user.signingKey = "/home/linda/.ssh/id_ed25519.pub";
+      gpg.format = "ssh";
+    };
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
