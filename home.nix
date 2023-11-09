4e19040e1b34170b842ca5cee1c4b565c981fb08
Linda Siemons <linda@catbrained.dev>
2023-11-09 23:45:28 +0100

    feat: configure ssh client via home manager
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home.nix
+++ home.nix
@@ -56,6 +56,11 @@
     enable = true;
   };
 
+  programs.ssh = {
+    enable = true;
+    forwardAgent = false;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
