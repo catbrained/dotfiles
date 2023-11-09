f896535079b51a72213836f1878146dfaa9b66e1
Linda Siemons <linda@catbrained.dev>
2023-11-09 05:09:57 +0100

    feat: enable kitty terminal emulator
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home.nix
+++ home.nix
@@ -19,6 +19,11 @@
     };
   };
 
+  # Terminal emulator
+  programs.kitty = {
+    enable = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
