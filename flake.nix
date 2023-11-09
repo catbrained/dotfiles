0e9608c046b70f986126b3cb8ca75839fb0ba96d
Linda Siemons <linda@catbrained.dev>
2023-11-09 03:17:41 +0100

    feat: add Home Manager
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       "quasar" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.linda = import ./home.nix;
+          }
         ];
       };
     };
