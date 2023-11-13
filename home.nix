catbrained/dotfiles

commit 4f7ec46584313f962bd7cdb9f9f4420c33300997

refactor(git): change signingKey to literal key
Signed-off-by: Linda Siemons <linda@catbrained.dev>

home.nix
@@ -14,7 +14,7 @@
       # XXX: The HM module does not yet support ssh signing
       commit.gpgSign = true;
       tag.gpgSign = true;
-      user.signingKey = "/home/linda/.ssh/id_ed25519.pub";
+      user.signingKey = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIAsoKQZS3EtdVd/wdw+m30ZAe5shaz1R+HENhwaVcHs linda@catbrained.dev";
       gpg.format = "ssh";
     };
   };

commit 47661dabb598bb514eafd57cf08687eb147c230f

fix(git): reference ssh key by filename
When using a literal key as `user.signingKey` git will try to use
ssh-agent to find the corresponding private key, which fails
when no ssh-agent is present.

Fixes: 4f7ec46584313f962bd7cdb9f9f4420c33300997

Signed-off-by: Linda Siemons <linda@catbrained.dev>

home.nix
@@ -14,7 +14,7 @@
       # XXX: The HM module does not yet support ssh signing
       commit.gpgSign = true;
       tag.gpgSign = true;
-      user.signingKey = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIAsoKQZS3EtdVd/wdw+m30ZAe5shaz1R+HENhwaVcHs linda@catbrained.dev";
+      user.signingKey = "~/.ssh/id_ed25519";
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.config/git/allowed_signers";
     };