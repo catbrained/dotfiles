10b66bd9dd9e95d6f5f92faaeccc27b4bc94aa96
Linda Siemons <linda@catbrained.dev>
2024-03-02 02:33:20 +0100

    style: format with nixpkgs-fmt
    nixpkgs-fmt was messing up my clean diff, but whatever...
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- flake.nix
+++ flake.nix
@@ -18,35 +18,35 @@
       ];
     in
     {
-    nixosConfigurations = {
-      "quasar" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          {
-            nixpkgs.config.allowUnfreePredicate = pkg:
-              builtins.elem (nixpkgs.lib.getName pkg) allowedUnfree;
-          }
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.linda = import ./home.nix;
-          }
-          {
-            # Make commands like `nix run nixpkgs#hello` use the same nixpkgs as this flake
-            # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
-            nix.registry.nixpkgs.flake = nixpkgs;
+      nixosConfigurations = {
+        "quasar" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              nixpkgs.config.allowUnfreePredicate = pkg:
+                builtins.elem (nixpkgs.lib.getName pkg) allowedUnfree;
+            }
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.linda = import ./home.nix;
+            }
+            {
+              # Make commands like `nix run nixpkgs#hello` use the same nixpkgs as this flake
+              # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
+              nix.registry.nixpkgs.flake = nixpkgs;
 
-            # Make `nix repl '<nixpkgs>'` use the same nixpkgs as this flake
-            # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
-            environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
-            # See: https://github.com/NixOS/nix/issues/9574
-            nix.settings.nix-path = nixpkgs.lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+              # Make `nix repl '<nixpkgs>'` use the same nixpkgs as this flake
+              # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
+              environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
+              # See: https://github.com/NixOS/nix/issues/9574
+              nix.settings.nix-path = nixpkgs.lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
 
-          }
-        ];
+            }
+          ];
+        };
       };
     };
-  };
 }
