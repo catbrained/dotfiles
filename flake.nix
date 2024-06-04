a8addee4e8247c796af9a5d2fa46b70a82e0a77a
Linda Siemons <linda@catbrained.dev>
2024-06-04 22:57:01 +0200

    fix: unset custom nix-path and flake registry
    This should no longer be necessary I think, because this is the
    default behaviour now. I think. Probably.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- flake.nix
+++ flake.nix
@@ -40,18 +40,6 @@
               home-manager.useUserPackages = true;
               home-manager.users.linda = import ./home.nix;
             }
-            {
-              # Make commands like `nix run nixpkgs#hello` use the same nixpkgs as this flake
-              # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
-              nix.registry.nixpkgs.flake = nixpkgs;
-
-              # Make `nix repl '<nixpkgs>'` use the same nixpkgs as this flake
-              # See: https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry#custom-nix-path-and-flake-registry-1
-              environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
-              # See: https://github.com/NixOS/nix/issues/9574
-              nix.settings.nix-path = nixpkgs.lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
-
-            }
           ];
         };
       };
