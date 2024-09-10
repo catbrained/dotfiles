72544b2d9746250ed7d96df9d9cdbddeda6ddd6d
Linda Siemons <linda@catbrained.dev>
2024-09-10 15:30:26 +0200

    feat: customize GTK theme
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home.nix
+++ home.nix
@@ -58,6 +58,25 @@ in
     . "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
   '';
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+  };
+
   xdg.mimeApps = {
     enable = true;
 
