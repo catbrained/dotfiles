9c22b39a38ee7af5f8ba2cf4f74b23f25e87bd95
Linda Siemons <linda@catbrained.dev>
2025-09-20 23:51:40 +0200

    feature: add virtual camera via v4l2loopback
    This lets me use OBS as a camera in apps like Discord or Teamspeak.
    This is useful especially because the new Teamspeak client currently
    seems to still be buggy with regards to streaming on Wayland (and maybe
    Linux in general).
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- configuration.nix
+++ configuration.nix
@@ -76,7 +76,18 @@
     initrd.systemd = {
       enable = true;
     };
+    extraModulePackages = [
+      config.boot.kernelPackages.v4l2loopback
+    ];
+    kernelModules = [
+      "v4l2loopback"
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
   };
+
+  security.polkit.enable = true;
 
   virtualisation.podman.enable = true;
 
