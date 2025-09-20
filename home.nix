96839935d5c1719e0772fb9476aff4d5b174d8ea
Linda Siemons <linda@catbrained.dev>
2025-09-21 00:50:46 +0200

    fix(ssh): replace deprecated default values
    HM will deprecate the default values for SSH, so I replaced them with
    the suggested explicit version.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home.nix
+++ home.nix
@@ -694,9 +694,20 @@
 
   programs.ssh = {
     enable = true;
-    forwardAgent = false;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "confirm 20m";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
     extraConfig = ''
-      AddKeysToAgent confirm 20m
       PasswordAuthentication no
     '';
   };
