6cd57c2d8efd1ba8643f7dbe38b534b1f83a18ab
Linda Siemons <linda@catbrained.dev>
2026-02-03 14:48:17 +0100

    refactor: move fzf config into separate file
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home/default.nix
+++ home/default.nix
@@ -27,6 +27,7 @@
     ./eza.nix
     ./fd.nix
     ./ripgrep.nix
+    ./fzf.nix
   ];
 
   home.username = "linda";
@@ -158,49 +159,6 @@
         end
       end
     '';
-  };
-
-  # A command-line fuzzy finder
-  programs.fzf = {
-    enable = true;
-    # The command that gets executed as the source for the ALT-C keybinding
-    changeDirWidgetCommand = "fd --type d";
-    # Command line options for the ALT-C keybinding
-    changeDirWidgetOptions = [
-      "--preview 'eza --color=always -T {} | head -200'"
-    ];
-    colors = {
-      "bg+" = "#313244";
-      bg = "#1e1e2e";
-      spinner = "#f5e0dc";
-      hl = "#f38ba8";
-      fg = "#cdd6f4";
-      header = "#f38ba8";
-      info = "#cba6f7";
-      pointer = "#f5e0dc";
-      marker = "#f5e0dc";
-      "fg+" = "#cdd6f4";
-      prompt = "#cba6f7";
-      "hl+" = "#f38ba8";
-    };
-    # The command that gets executed as the default source
-    defaultCommand = "fd --type f";
-    # The options given to fzf by default
-    defaultOptions = [
-      "--height 40%"
-      "--border"
-      "--layout=reverse"
-    ];
-    # The command that gets executed as the source for the CTRL-T keybinding
-    fileWidgetCommand = "fd --type f";
-    # The options for CTRL-T
-    fileWidgetOptions = [
-      "--preview 'bat --line-range :20 --color always {}'"
-    ];
-    # Command line options for the CTRL-R keybind
-    historyWidgetOptions = [
-      "--scheme=history"
-    ];
   };
 
   # Screen locker
