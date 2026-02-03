c715cc57018a928015cab7d0100409ea07665e41
Linda Siemons <linda@catbrained.dev>
2026-02-03 14:37:27 +0100

    refactor: move kitty config into separate file
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home/default.nix
+++ home/default.nix
@@ -19,6 +19,7 @@
     ./helix.nix
     ./bat.nix
     ./fish.nix
+    ./kitty.nix
   ];
 
   home.username = "linda";
@@ -189,35 +190,6 @@
   programs.starship = {
     enable = true;
     settings = { };
-  };
-
-  # Terminal emulator
-  programs.kitty = {
-    enable = true;
-    themeFile = "Catppuccin-Mocha";
-    settings = {
-      font_family = "Iosevka";
-      bold_font = "Iosevka Bold";
-      italic_font = "Iosevka Italic";
-      bold_italic_font = "Iosevka Bold Italic";
-      font_size = "13.5";
-      enabled_layouts = "tall:bias=65;full_size=1;mirrored=true, fat:bias=65;full_size=1;mirrored=false, grid, splits:split:axis=horizontal, horizontal, vertical, stack";
-      tab_bar_style = "powerline";
-      tab_powerline_style = "round";
-    };
-    keybindings = {
-      "ctrl+alt+enter" = "launch --cwd=current";
-      "ctrl+alt+t" = "new_tab_with_cwd";
-      "ctrl+alt+z" = "toggle_layout stack";
-      "ctrl+alt+h" = "neighboring_window left";
-      "ctrl+alt+j" = "neighboring_window down";
-      "ctrl+alt+k" = "neighboring_window up";
-      "ctrl+alt+l" = "neighboring_window right";
-      "ctrl+alt+shift+h" = "move_window left";
-      "ctrl+alt+shift+j" = "move_window down";
-      "ctrl+alt+shift+k" = "move_window up";
-      "ctrl+alt+shift+l" = "move_window right";
-    };
   };
 
   # A ls replacement
