61dae4eee6481201d127458b87d1be4e818a6614
Linda Siemons <linda@catbrained.dev>
2026-02-11 16:05:20 +0100

    feat: remove unused custom ISO stuff
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- flake.nix
+++ flake.nix
@@ -73,14 +73,6 @@
             }
           ];
         };
-
-        # Build it with `nix run nixpkgs#nixos-generators -- --format iso --flake .#customIso -o result`
-        customIso = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/customIso/configuration.nix
-          ];
-        };
       };
     };
 }
