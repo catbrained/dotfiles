5a57db4076ffdf4b0ebf510e475cc6918908413d
Linda Siemons <linda@catbrained.dev>
2026-04-27 15:42:59 +0200

    fix(pulsar): allow root login via ssh
    Doing a remote deploy via a regular user was giving me
    trouble. Hopefully this fixes it.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- hosts/pulsar/ssh.nix
+++ hosts/pulsar/ssh.nix
@@ -6,14 +6,17 @@
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
       AllowUsers = [
+        "root"
         "linda"
       ];
     };
   };
 
   users.users.linda.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIAsoKQZS3EtdVd/wdw+m30ZAe5shaz1R+HENhwaVcHs linda@catbrained.dev"
+  ];
+  users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIAsoKQZS3EtdVd/wdw+m30ZAe5shaz1R+HENhwaVcHs linda@catbrained.dev"
   ];
 }
