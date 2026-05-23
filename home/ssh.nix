f61ef56530caa02c3771f5805cb0366d90f7a8d9
Linda Siemons <linda@catbrained.dev>
2026-05-23 16:25:12 +0200

    chore(ssh): replace deprecated `matchBlocks` syntax
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,21 +4,23 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "confirm 20m";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-      };
-      matchBlocks."pulsar" = {
-        hostname = "89.58.12.21";
-        port = 52783;
+      settings = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "confirm 20m";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+        "pulsar" = {
+          hostname = "89.58.12.21";
+          port = 52783;
+        };
       };
       extraConfig = ''
         PasswordAuthentication no
